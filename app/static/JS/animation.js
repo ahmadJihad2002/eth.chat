47a69367b696c21afe8af51c733deb9af6c55768
ahmadJihad2002 <jhadmhmad38@gmail.com>
2022-12-06 21:25:17 +0200

    i connect the backend with frontend

---
+++ app/static/JS/animation.js
@@ -0,0 +1,98 @@
+
+/* ---- particles.js config ---- */
+particlesJS("particles-js", {
+
+   "particles": {
+    "number": {
+      "value": 300,
+      "density": {
+      "enable": false,
+        "value_area": 1000
+      }
+    },
+    "color": {
+      "value": ["#356797", "#356797"]
+    },
+    "shape": {
+      "type": "polygon",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 4
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "size_min": 1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#58636d",
+      "opacity": .8,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": .5,
+      "direction": "left",
+      "random": true,
+      "straight": true,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 200,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
--- app/views.py
+++ app/views.py
@@ -1,35 +1,61 @@
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, url_for
 from app import app
 import deploy_smart_contract
 
+network_info = {"HTTP": None, "ID": None, "myAddress": None}
 
-@app.route('/mainPage')
+user_info = {'friendList': None, 'friendMsg': None}
+
+
+@app.route('/')
 def mainPage():
-    # chat=deploy_smart_contract.SmartContract( )
-
     return render_template("mainPage.html")
 
 
-@app.route('/chatPage', methods=["GET", "POST"])
-def home():
+@app.route('/chat_page', methods=["GET", "POST"])
+def chat_page():
+    friend_list = None
     if request.method == "POST":
         req = request.form
-        return redirect(request.url)
-    return render_template("chatPage.html")
+        address = req['friendAddress']
+        name = req['friendName']
+        user.addFriend(friendName=name, friendAddress=address)
+        friend_list = user.showfriendList()
+        print("friend been added")
+    return render_template("chatPage.html", friend_list=friend_list,
+                           is_connected=network_info["HTTP"])
 
 
 @app.route('/network', methods=["GET", "POST"])
 def network():
     if request.method == "POST":
         req = request.form
-        req["ID"]
-        req["network_name"]
-        req["HTTP"]
-        req["privateKey"]
-        req["publicKey"]
-        req["username"]
+        network_info["HTTP"] = req["HTTP"]
+        network_info["ID"] = req["ID"]
+        network_info["myAddress"] = req["publicKey"]
+        global user
+        user = deploy_smart_contract.SmartContract(HTTP=req["HTTP"], chainId=int(req["ID"]),
+                                                   privateKey=req["privateKey"],
+                                                   publicKey=req["publicKey"], name=req["username"])
+        if user.checkUserExists(Address=network_info["myAddress"]):
+            return redirect(url_for("chat_page"))
 
-        deploy_smart_contract.SmartContract(HTTP=req["HTTP"], chainId=req["ID"], privateKey=req["privateKey"],
-                                            publicKey=req["publicKey"], username=req["username"])
+    return render_template("network.html", network_info=network_info)
 
-    return render_template("network.html")
+
+@app.route('/message/<string:friend_address>', methods=["GET", "POST"])
+def msg(friend_address):
+    friend_address = friend_address
+    if request.method == "POST":
+        req = request.form
+        user.sendMessage(receiverAddress=friend_address, msg=req['msg'])
+    return render_template("msg.html", receiver=friend_address,
+                           msgs=user.readMessages(friend_address))
+
+#
+# def chick_if_connected():
+#     pass
+
+# @app.errorhandler(404)
+# def error():
+#     return render_template("errors/logInErrors.html")
